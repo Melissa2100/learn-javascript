527a024a8363bbd7244dd0d54979749a16db89dc
Blessing Awak <melissasignature1@gmail.com>
2023-11-30 00:15:24 +0100

    commit

--- index.js
+++ index.js
@@ -7,9 +7,16 @@
 // 2. Assignment (assignment operator)
 
 //3 data types
-let firstName = "Pascal"; //strings
-let age = 21; //number
-let student = true; //boolean
-document.getElementById("p1").innerHTML = "Hello " + firstName;
-document.getElementById("p2").innerHTML = "You are " + age + " years old";
-document.getElementById("p3").innerHTML = "Enrolled: " + student;
+// let firstName = "Pascal"; //strings
+// let age = 21; //number
+// let student = true; //boolean
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p2").innerHTML = "You are " + age + " years old";
+// document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+let username;
+document.getElementById("myButton").onclick = function () {
+  username = document.getElementById("myText").value;
+  console.log(username);
+  document.getElementById("myLabel").innerHTML = "Hello " + username;
+};
