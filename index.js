2755cb2e83c68b028b133606c99cc980d29e2c78
Blessing Awak <melissasignature1@gmail.com>
2023-11-29 23:52:28 +0100

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstName = 'Pascal';
-
-document.getElementById("p1").innerHTML = "Hello" + ;
-document.getElementById("p2");
-document.getElementById("p3");
+let firstName = "Pascal";
+let age = 21;
+let student = true;
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + student;
