417cfa9b9c50093f6a650b9f7b11e5166cbf83a7
Blessing Awak <melissasignature1@gmail.com>
2023-12-07 21:53:49 +0100

    commit

--- index.js
+++ index.js
@@ -44,3 +44,15 @@
 
 // c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 // console.log("side C:", c);
+
+document.getElementById("submitButton").onclick = function() {
+  a = document.getElementById("aTextBox").value;
+  a = Number(a);
+
+  b = document.getElementById("bTextBox").value;
+  b = Number(b);
+
+  c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+  document.getElementById("cLabel").innerHTML = "Side C: " + c;
+};
