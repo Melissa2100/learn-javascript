9991b627504e07bd88be192a605fc024ba936da4
Blessing Awak <melissasignature1@gmail.com>
2023-12-07 22:14:50 +0100

    commit

--- index.js
+++ index.js
@@ -45,14 +45,31 @@
 // c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 // console.log("side C:", c);
 
-document.getElementById("submitButton").onclick = function() {
-  a = document.getElementById("aTextBox").value;
-  a = Number(a);
+// document.getElementById("submitButton").onclick = function() {
+//   a = document.getElementById("aTextBox").value;
+//   a = Number(a);
 
-  b = document.getElementById("bTextBox").value;
-  b = Number(b);
+//   b = document.getElementById("bTextBox").value;
+//   b = Number(b);
 
-  c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//   c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 
-  document.getElementById("cLabel").innerHTML = "Side C: " + c;
+//   document.getElementById("cLabel").innerHTML = "Side C: " + c;
+// };
+
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function () {
+  count -= 1;
+  document.getElementById("countLabel").innerHTML = count;
+};
+
+document.getElementById("resetBtn").onclick = function () {
+  count = 0;
+  document.getElementById("countLabel").innerHTML = count;
+};
+
+document.getElementById("increaseBtn").onclick = function () {
+  count += 1;
+  document.getElementById("countLabel").innerHTML = count;
 };
