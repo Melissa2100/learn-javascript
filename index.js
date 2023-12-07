7c23ab27a6c9b55007c012915ab216ba1cdb88e0
Blessing Awak <melissasignature1@gmail.com>
2023-12-07 23:23:03 +0100

    COMMIT

--- index.js
+++ index.js
@@ -57,19 +57,19 @@
 //   document.getElementById("cLabel").innerHTML = "Side C: " + c;
 // };
 
-let count = 0;
+// let count = 0;
 
-document.getElementById("decreaseBtn").onclick = function () {
-  count -= 1;
-  document.getElementById("countLabel").innerHTML = count;
-};
+// document.getElementById("decreaseBtn").onclick = function () {
+//   count -= 1;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
 
-document.getElementById("resetBtn").onclick = function () {
-  count = 0;
-  document.getElementById("countLabel").innerHTML = count;
-};
+// document.getElementById("resetBtn").onclick = function () {
+//   count = 0;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
 
-document.getElementById("increaseBtn").onclick = function () {
-  count += 1;
-  document.getElementById("countLabel").innerHTML = count;
-};
+// document.getElementById("increaseBtn").onclick = function () {
+//   count += 1;
+//   document.getElementById("countLabel").innerHTML = count;
+// };
