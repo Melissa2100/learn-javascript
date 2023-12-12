cb5d9b7419ba2b9ed94294076a52d958c80c6545
Blessing Awak <melissasignature1@gmail.com>
2023-12-12 18:50:35 +0100

    commit

--- index.js
+++ index.js
@@ -73,3 +73,18 @@
 //   count += 1;
 //   document.getElementById("countLabel").innerHTML = count;
 // };
+
+// math
+let x;
+let y;
+let z;
+
+document.getElementById("rollButton").onclick = function () {
+  x = Math.floor(Math.random() * 6) + 1;
+  y = Math.floor(Math.random() * 6) + 1;
+  z = Math.floor(Math.random() * 6) + 1;
+
+  document.getElementById("xlabel").innerHTML = x;
+  document.getElementById("ylabel").innerHTML = y;
+  document.getElementById("zlabel").innerHTML = z;
+ };
