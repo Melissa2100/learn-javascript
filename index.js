3345e74ba4b181d05b876038aec599ad009bc851
Blessing Awak <melissasignature1@gmail.com>
2023-12-21 18:43:36 +0100

    commit

--- index.js
+++ index.js
@@ -189,10 +189,18 @@
 
 // console.log("HAPPY NEW YEAR!");
 
-// break
+// // break
+// for (let i = 1; i <= 20; i += 1) {
+//   if (i == 13) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// continue
 for (let i = 1; i <= 20; i += 1) {
   if (i == 13) {
-    break;
+    continue;
   }
   console.log(i);
 }
