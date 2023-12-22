4335c4a24944dbfcea1c719e296c2a7f1a14e484
Blessing Awak <melissasignature1@gmail.com>
2023-12-22 22:56:49 +0100

    commit

--- index.js
+++ index.js
@@ -210,19 +210,19 @@
 //   console.log(i);
 // }
 
-// return
-let area;
-let width;
-let height;
+// // return
+// let area;
+// let width;
+// let height;
 
-width = window.prompt("Enter width");
-height = window.prompt("Enter height");
+// width = window.prompt("Enter width");
+// height = window.prompt("Enter height");
 
-area = getArea(width, height);
+// area = getArea(width, height);
 
-console.log("The area is", area);
+// console.log("The area is:", area);
 
-function getArea(width, height) {
-  let result = width * height;
-  return result;
-}
+// function getArea(width, height) {
+//   let result = width * height;
+//   return result;
+// }
