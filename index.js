7459e8de2fd51268a54a3c1f5557869509ec19c3
Blessing Awak <melissasignature1@gmail.com>
2023-12-23 17:25:36 +0100

    commit

--- index.js
+++ index.js
@@ -241,6 +241,12 @@ let userName = "Bro";
 let items = 3;
 let total = 75;
 
-console.log(`Hello, ${userName}!`);
-console.log(`You have, ${items} items in your cart`);
-console.log(`Your total is $${total}`);
+// console.log(`Hello, ${userName}!`);
+// console.log(`You have, ${items} items in your cart`);
+// console.log(`Your total is $${total}`)
+
+let text = `Hello ${userName}!
+You have, ${items} items in your cart
+Your total is $${total}.`;
+
+console.log(text);
