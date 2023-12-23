dcad43d472624dd9a8ddc35adebb83cf7d0adb07
Blessing Awak <melissasignature1@gmail.com>
2023-12-23 17:45:40 +0100

    commit

--- index.js
+++ index.js
@@ -260,10 +260,18 @@
 // 'locale' = specify that language (undefined = default set in)
 // 'option' = object with formatting options
 
-let myNum = 123456.789;
+let myNum = 100;
 
 myNum = myNum.toLocaleString("en-US"); // US English
 myNum = myNum.toLocaleString("hi-IN"); // Hindi
 myNum = myNum.toLocaleString("de-DE"); // standard German
+
+myNum = myNum.toLocaleString("en-US", { style: "currency", currency: "USD" });
+myNum = myNum.toLocaleString("hi-IN", { style: "currency", currency: "INR" });
+myNum = myNum.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
+
+myNum = myNum.toLocaleString(undefined, { style: "percent" });
+
+myNum = myNum.toLocaleString(undefined, { style: "unit", unit: "celsius" });
 
 console.log(myNum);
