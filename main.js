0db00637fa11ae73ddfc2141d0babbe20d603d80
Blessing Awak <melissasignature1@gmail.com>
2023-12-26 15:31:03 +0100

    commit

--- main.js
+++ main.js
@@ -15,9 +15,23 @@
 // };
 
 // converting temp
-let temp = 32;
-temp = toFahrenheit(temp);
-console.log(temp);
+document.getElementById("submitButton").onclick = function () {
+  let temp;
+
+  if (document.getElementById("cButton").checked) {
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+    temp = toCelsius(temp);
+    document.getElementById("tempLabel").innerHTML = temp + "°C";
+  } else if (document.getElementById("fButton").checked) {
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+    temp = toFahrenheit(temp);
+    document.getElementById("tempLabel").innerHTML = temp + "°F";
+  } else {
+    document.getElementById("tempLabel").innerHTML = "Select a unit";
+  }
+};
 
 function toCelsius(temp) {
   return (temp - 32) * (5 / 9);
