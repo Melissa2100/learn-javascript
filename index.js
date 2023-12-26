d276c29a6a79fdfa5629629bb54be103163e4a30
Blessing Awak <melissasignature1@gmail.com>
2023-12-26 19:23:03 +0100

    commit

--- index.js
+++ index.js
@@ -292,12 +292,12 @@
 // console.log(index);
 
 // looping through an array
-let prices = [5, 10, 15, 20];
+let prices = [5, 10, 15, 20, 25];
 
 // for(let i = prices.length - 1; i >= 0; i-=1) {
 //  console.log(prices[i]);
 // }
 
-for (let i of prices) {
-  console.log(i);
+for (let price of prices) {
+  console.log(price);
 }
