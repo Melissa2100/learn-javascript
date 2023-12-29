8f4169b17771faaa1ac694df0a92a413b77d3746
Blessing Awak <melissasignature1@gmail.com>
2023-12-29 19:52:06 +0100

    commit

--- index.js
+++ index.js
@@ -386,9 +386,14 @@
 // array.forEach()
 let students = ["charles", "blessing", "love"];
 students.forEach(capitalize);
+students.forEach(print);
 
 function capitalize(element, index, array) {
-  array[index] = element[0].toUpperCase() + element.substring(1 );
+  array[index] = element[0].toUpperCase() + element.substring(1);
 }
 
 console.log(students[0]);
+
+function print(element) {
+  console.log(element);
+}
