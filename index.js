400cecfd7434020525255ca0b852252f7294e21f
Blessing Awak <melissasignature1@gmail.com>
2023-12-29 20:13:27 +0100

    commit

--- index.js
+++ index.js
@@ -400,15 +400,17 @@
 
 // array.map
 let numbers = [1, 2, 3, 4, 5];
-let squares = numbers.map(square);
+// let squares = numbers.map(square);
+
 let cubes = numbers.map(cube);
 
-squares.forEach(print);
+// squares.forEach(print);
+
 cubes.forEach(print);
 
-function square(element) {
-  return Math.pow(element, 2);
-}
+// function square(element) {
+//   return Math.pow(element, 2);
+// }
 
 function cube(element) {
   return Math.pow(element, 3);
