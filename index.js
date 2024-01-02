0e4e5152346781d718589264e81b7317159d8ef7
Blessing Awak <melissasignature1@gmail.com>
2024-01-02 19:59:22 +0100

    commit

--- index.js
+++ index.js
@@ -497,5 +497,9 @@ function shuffle(array) {
   while (currentIndex != 0) {
     let randomIndex = Math.floor(Math.random() * array.length);
     currentIndex -= 1;
+
+    let temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
   }
 }
