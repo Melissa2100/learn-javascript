44d39c4ff4ca4b48f2ddd761e469d72083823b5b
Blessing Awak <melissasignature1@gmail.com>
2024-01-03 14:14:26 +0100

    commit

--- index.js
+++ index.js
@@ -517,9 +517,11 @@ login();
 function login() {
   displayUserName();
   displayUserInbox();
+
   function displayUserName() {
     console.log(`Welcome ${userName}`);
   }
+
   function displayUserInbox() {
     console.log(`You have ${userInbox} new messages`);
   }
