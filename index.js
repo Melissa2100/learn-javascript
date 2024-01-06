6bde660b3f7d15c4bebc7c9b06cebf6a7e95a80d
Blessing Awak <melissasignature1@gmail.com>
2024-01-06 23:39:09 +0100

    commit

--- index.js
+++ index.js
@@ -826,9 +826,19 @@ class Car {
   get gas() {
     return `${this._gas}L (${(this._gas / 50) * 100}%)`;
   }
+  set gas(value) {
+    if (value > 50) {
+      value = 50;
+    } else if (value < 0) {
+      value = 0;
+    }
+    this._gas = value;
+  }
 }
 
 let car = new Car(400);
+
+car.gas = -100;
 
 console.log(car.power);
 console.log(car.gas);
