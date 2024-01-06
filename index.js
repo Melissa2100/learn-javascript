6c182b0f706e2880644777828004279265bc3010
Blessing Awak <melissasignature1@gmail.com>
2024-01-06 23:34:17 +0100

    commit

--- index.js
+++ index.js
@@ -817,13 +817,18 @@
 // get
 class Car {
   constructor(power) {
+    this._gas = 50;
     this._power = power;
   }
   get power() {
     return `${this._power}hp`;
+  }
+  get gas() {
+    return `${this._gas}L (${(this._gas / 50) * 100}%)`;
   }
 }
 
 let car = new Car(400);
 
 console.log(car.power);
+console.log(car.gas);
