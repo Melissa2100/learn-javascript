13b415ca2e3d4c1e082824e8400bf99cf8218ef3
Blessing Awak <melissasignature1@gmail.com>
2024-01-06 23:52:32 +0100

    commit

--- index.js
+++ index.js
@@ -856,10 +856,15 @@ const car1 = new Car("Mustang", 2023, "red");
 const car2 = new Car("Corvette", 2024, "blue");
 const car3 = new Car("Lambo", 2022, "yellow");
 
+changeColor(car3, "purple");
 displayInfo(car3);
 
 function displayInfo(car) {
   console.log(car.model);
   console.log(car.year);
   console.log(car.color);
+}
+
+function changeColor(car, color) {
+  car.color = color;
 }
