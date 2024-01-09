56306b5aa51e9cfa30c1ae7baff764001b8490a6
Blessing Awak <melissasignature1@gmail.com>
2024-01-09 18:58:00 +0100

    commit

--- index.js
+++ index.js
@@ -869,4 +869,21 @@
 //   car.color = color;
 // }
 
+// Array of objects
+class Car {
+  constructor(model, year, color) {
+    this.model = model;
+    this.year = year;
+    this.color = color;
+  }
 
+  drive() {
+    console.log(`You drive the ${this.model}`);
+  }
+}
+
+const car1 = new Car("Mustang", 2023, "red");
+const car2 = new Car("Corvette", 2024, "blue");
+const car3 = new Car("Lambo", 2022, "yellow");
+
+const cars = [car1, car2, car3];
