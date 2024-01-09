07d179b5f5258453730dd1a50c395fa0681fda3f
Blessing Awak <melissasignature1@gmail.com>
2024-01-09 20:04:51 +0100

    commit

--- index.js
+++ index.js
@@ -869,35 +869,44 @@
 //   car.color = color;
 // }
 
-// Array of objects
-class Car {
-  constructor(model, year, color) {
-    this.model = model;
-    this.year = year;
-    this.color = color;
-  }
+// // Array of objects
+// class Car {
+//   constructor(model, year, color) {
+//     this.model = model;
+//     this.year = year;
+//     this.color = color;
+//   }
 
-  drive() {
-    console.log(`You drive the ${this.model}`);
-  }
-}
+//   drive() {
+//     console.log(`You drive the ${this.model}`);
+//   }
+// }
 
-const car1 = new Car("Mustang", 2023, "red");
-const car2 = new Car("Corvette", 2024, "blue");
-const car3 = new Car("Lambo", 2022, "yellow");
-const car4 = new Car("Ferrari", 2025, "white");
+// const car1 = new Car("Mustang", 2023, "red");
+// const car2 = new Car("Corvette", 2024, "blue");
+// const car3 = new Car("Lambo", 2022, "yellow");
+// const car4 = new Car("Ferrari", 2025, "white");
 
-const cars = [car1, car2, car3, car4];
+// const cars = [car1, car2, car3, car4];
 
-// cars[0].drive();
-// cars[1].drive();
-// cars[2].drive();
+// // cars[0].drive();
+// // cars[1].drive();
+// // cars[2].drive();
 
-// a function to loop over the element of the array "const cars = []" and invoke the drive() method of each element.
-startRace(cars);
+// // a function to loop over the element of the array "const cars = []" and invoke the drive() method of each element.
+// startRace(cars);
 
-function startRace(cars) {
-  for (const car of cars) {
-    car.drive();
+// function startRace(cars) {
+//   for (const car of cars) {
+//     car.drive();
+//   }
+// }
+
+// anonymous objects
+
+class Cards {
+  constructor(value, suit) {
+    this.value = value;
+    this.suit = suit;
   }
 }
