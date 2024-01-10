259332b97f3c80e76f862bb243b63b6935303542
Blessing Awak <melissasignature1@gmail.com>
2024-01-10 18:50:09 +0100

    commit

--- index.js
+++ index.js
@@ -904,9 +904,22 @@
 
 // anonymous objects
 
-class Cards {
+class Card {
   constructor(value, suit) {
     this.value = value;
     this.suit = suit;
   }
 }
+
+let card1 = new Card("A", "Hearts");
+let card2 = new Card("A", "Spades");
+let card3 = new Card("A", "Diamonds");
+let card4 = new Card("A", "Clubs");
+let card5 = new Card("2", "Hearts");
+let card6 = new Card("2", "Spades");
+let card7 = new Card("2", "Diamonds");
+let card8 = new Card("2", "Clubs");
+
+let cards = [card1, card2, card3, card4, card5, card6, card7, card8];
+
+console.log(card1.value + card1.suit);
