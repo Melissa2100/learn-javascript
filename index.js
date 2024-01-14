ff76316f011a67b60eee01ef81e27cf6ba65504a
Blessing Awak <melissasignature1@gmail.com>
2024-01-14 20:41:18 +0100

    commit

--- index.js
+++ index.js
@@ -1103,16 +1103,16 @@
 // console.log(area);
 
 // // DOM
-// // console.log(document.title);
-// // console.log(document.URL);
+// console.log(document.title);
+// console.log(document.URL);
 
-// // document.title = "Title goes here";
-// // document.location = "https://www.google.com";
+// document.title = "Title goes here";
+// document.location = "https://www.google.com";
 
 // document.body.style.backgroundColor = "skyblue";
 // document.getElementById("myDiv").innerHTML = "Hello";
 
-// how to select element in a web page
+// // how to select element in a web page
 // let element = document.getElementById("myTitle");
 // element.style.backgroundColor = "lightgreen";
 
@@ -1130,5 +1130,12 @@
 // let desserts = document.getElementsByClassName("desserts");
 // desserts[2].style.backgroundColor = "lightgreen";
 
-let element = document.querySelector("#myTitle");
-element.style.backgroundColor = "lightgreen";
+// let element = document.querySelector("[for]");
+// element.style.backgroundColor = "lightgreen";
+
+let elements = document.querySelectorAll("li");
+elements.forEach((element) => {
+  element.style.backgroundColor = "lightgreen";
+});
+
+
