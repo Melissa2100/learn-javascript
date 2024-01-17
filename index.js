cdfda5286ebdad64db26a3b239a40c65f49bc662
Blessing Awak <melissasignature1@gmail.com>
2024-01-17 20:59:57 +0100

    commit

--- index.js
+++ index.js
@@ -1201,14 +1201,16 @@
 //   this.style.backgroundColor = "lightblue";
 // }
 
-// show and hide HTML element
-const myButton = document.querySelector("#myButton");
-const myImg = document.querySelector("#myImg");
+// // show and hide HTML element
+// const myButton = document.querySelector("#myButton");
+// const myImg = document.querySelector("#myImg");
 
-myButton.addEventListener("click", () => {
-  if (myImg.style.visibility == "hidden") {
-    myImg.style.visibility = "visible";
-  } else {
-    myImg.style.visibility = "hidden";
-  }
-});
+// myButton.addEventListener("click", () => {
+//   if (myImg.style.visibility == "hidden") {
+//     myImg.style.visibility = "visible";
+//   } else {
+//     myImg.style.visibility = "hidden";
+//   }
+// });
+
+// detect key presses
