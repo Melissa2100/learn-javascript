f249b3cdd50fade9c7e0519993c8b050261b9464
Blessing Awak <melissasignature1@gmail.com>
2024-01-20 06:13:34 +0100

    commit

--- index.js
+++ index.js
@@ -1241,3 +1241,26 @@
 // }
 
 // Animation
+const myButton = document.getElementById("myButton");
+const myAnimation = document.getElementById("myDiv");
+
+myButton.addEventListener("click", begin);
+
+function begin() {
+  let timerId = null;
+  let x = 0;
+  let y = 0;
+
+  setInterval(frame, 5);
+
+  function frame() {
+    if (x >= 200 || y >= 200) {
+      clearInterval(timerId);
+    } else {
+      x += 1;
+      y += 1;
+      myAnimation.style.left = x + "px";
+      myAnimation.style.top = y + "px";
+    }
+  }
+}
