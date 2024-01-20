aa19102eb9d584fb6a63f0ca7c1fb882c93554db
Blessing Awak <melissasignature1@gmail.com>
2024-01-20 05:58:45 +0100

    commiit

--- index.js
+++ index.js
@@ -1213,29 +1213,31 @@
 //   }
 // });
 
-// detect key presses
-const myDiv = document.getElementById("myDiv");
-window.addEventListener("keydown", move);
-let x = 0;
-let y = 0;
+// // detect key presses
+// const myDiv = document.getElementById("myDiv");
+// window.addEventListener("keydown", move);
+// let x = 0;
+// let y = 0;
 
-function move(event) {
-  switch (event.key) {
-    case "ArrowDown":
-      y += 5;
-      myDiv.style.top = y + "px";
-      break;
-    case "ArrowUp":
-      y -= 5;
-      myDiv.style.top = y + "px";
-      break;
-    case "ArrowRight":
-      x += 5;
-      myDiv.style.left = x + "px";
-      break;
-    case "ArrowLeft":
-      x -= 5;
-      myDiv.style.left = x + "px";
-      break;
-  }
-}
+// function move(event) {
+//   switch (event.key) {
+//     case "ArrowDown":
+//       y += 5;
+//       myDiv.style.top = y + "px";
+//       break;
+//     case "ArrowUp":
+//       y -= 5;
+//       myDiv.style.top = y + "px";
+//       break;
+//     case "ArrowRight":
+//       x += 5;
+//       myDiv.style.left = x + "px";
+//       break;
+//     case "ArrowLeft":
+//       x -= 5;
+//       myDiv.style.left = x + "px";
+//       break;
+//   }
+// }
+
+// Animation
