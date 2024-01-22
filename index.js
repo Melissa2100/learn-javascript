6ecd75c26593319fbcb87c33d318b6a88fef13f7
Blessing Awak <melissasignature1@gmail.com>
2024-01-22 15:38:05 +0100

    commit

--- index.js
+++ index.js
@@ -1248,19 +1248,51 @@ myButton.addEventListener("click", begin);
 
 function begin() {
   let timerId = null;
-  let x = 0;
-  let y = 0;
+  // let x = 0;
+  // let y = 0;
+
+  //// rotation
+  // let degrees = 0;
+  // let x = 0;
+  // let y = 0;
+
+  // scaling the element
+  let scaleX = 1; //1 means 100%
+  let scaleY = 1; //1 means 100%
 
   setInterval(frame, 5);
 
   function frame() {
-    if (x >= 200 || y >= 200) {
-      clearInterval(timerId);
-    } else {
-      x += 1;
-      y += 1;
-      myAnimation.style.left = x + "px";
-      myAnimation.style.top = y + "px";
+    // if (x >= 200 || y >= 200) {
+    //   clearInterval(timerId);
+    // } else {
+    //   x += 1;
+    //   y += 1;
+    //   myAnimation.style.left = x + "px";
+    //   myAnimation.style.top = y + "px";
+    // }
+
+    //// rotation
+    // if (x >= 500 || y >= 500) {
+    //   clearInterval(timerId);
+    // } else {
+    //   degrees += 3;
+    //   x += 1;
+    //   y += 1;
+    //   myAnimation.style.left = x + "px";
+    //   myAnimation.style.top = y + "px";
+    //   myAnimation.style.transform = "rotateZ(" + degrees + "deg)";
+    // }
+
+    // scaling the element
+    function frame() {
+      if (scaleX >= 2 || scaleY >= 2) {
+        clearInterval(timerId);
+      } else {
+        scaleX += 0.01;
+        scaleY += 0.01;
+        myAnimation.style.transform = "scale(" + scaleX + "," + scaleY + ")";
+      }
     }
   }
 }
