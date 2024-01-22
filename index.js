ba3b0b6cde7e9f779d3c7e5d3be961441cf56ef0
Blessing Awak <melissasignature1@gmail.com>
2024-01-22 15:57:46 +0100

    commit

--- index.js
+++ index.js
@@ -1301,7 +1301,12 @@
 let canvas = document.getElementById("myCanvas");
 let context = canvas.getContext("2d");
 
+context.strokeStyle = "gold";
+context.lineWidth = 10;
 context.beginPath();
 context.moveTo(0, 0);
+context.lineTo(250, 250);
+context.lineTo(250, 500);
+context.moveTo(500, 0);
 context.lineTo(250, 250);
 context.stroke();
