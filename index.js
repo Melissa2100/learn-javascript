11b72480878a07fa6182e7d952d454fd1b437f5d
Blessing Awak <melissasignature1@gmail.com>
2024-01-28 13:21:51 +0100

    commit

--- index.js
+++ index.js
@@ -1448,3 +1448,28 @@ cookieBtn.addEventListener("click", () => {
 
 deleteCookie("firstName");
 deleteCookie("lastName");
+
+function setCookie(name, value, daysToLive) {
+  const date = new Date();
+  date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+
+function deleteCookie(name) {
+  setCookie(name, null, null);
+}
+
+function getCookie(name) {
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArray = cDecoded.split(";");
+  console.log(cArray);
+  let result = null;
+
+  cArray.forEach((element) => {
+    if (element.indexOf(name) == 0) {
+      result = element.substring(name.length + 1);
+    }
+  });
+  return result;
+}
