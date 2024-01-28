5c604063550342fcab66dd1ddf052da4ec71e86a
Blessing Awak <melissasignature1@gmail.com>
2024-01-28 17:13:18 +0100

    commit

--- index.js
+++ index.js
@@ -1384,23 +1384,23 @@
 
 // cookies
 
-const firstName = document.querySelector("#firstText");
-const lastName = document.querySelector("#lastText");
-const submitBtn = document.querySelector("#submitBtn");
-const cookieBtn = document.querySelector("#cookieBtn");
+// const firstName = document.querySelector("#firstText");
+// const lastName = document.querySelector("#lastText");
+// const submitBtn = document.querySelector("#submitBtn");
+// const cookieBtn = document.querySelector("#cookieBtn");
 
-submitBtn.addEventListener("click", () => {
-  setCookie("firstName", firstText.value, 365);
-  setCookie("lastName", lastText.value, 365);
-});
+// submitBtn.addEventListener("click", () => {
+//   setCookie("firstName", firstText.value, 365);
+//   setCookie("lastName", lastText.value, 365);
+// });
 
-cookieBtn.addEventListener("click", () => {
-  firstText.value = getCookie("firstName");
-  lastText.value = getCookie("lastName");
-});
+// cookieBtn.addEventListener("click", () => {
+//   firstText.value = getCookie("firstName");
+//   lastText.value = getCookie("lastName");
+// });
 
-deleteCookie("firstName");
-deleteCookie("lastName");
+// deleteCookie("firstName");
+// deleteCookie("lastName");
 
 // console.log(navigator.cookieEnabled);
 // document.cookie =
@@ -1423,32 +1423,44 @@ deleteCookie("lastName");
 // console.log(getCookie("firstName"));
 // console.log(getCookie("lastName"));
 
-function setCookie(name, value, daysToLive) {
-  const date = new Date();
-  date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + date.toUTCString();
-  document.cookie = `${name}=${value}; ${expires}; path=/`;
-}
+// function setCookie(name, value, daysToLive) {
+//   const date = new Date();
+//   date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000));
+//   let expires = "expires=" + date.toUTCString();
+//   document.cookie = `${name}=${value}; ${expires}; path=/`;
+// }
 
-// delete cookie
-function deleteCookie(name) {
-  setCookie(name, null, null);
-}
+// // delete cookie
+// function deleteCookie(name) {
+//   setCookie(name, null, null);
+// }
 
-// getting value of cookie by name
-function getCookie(name) {
-  const cDecoded = decodeURIComponent(document.cookie);
-  const cArray = cDecoded.split(";");
-  console.log(cArray);
-  let result = null;
+// // getting value of cookie by name
+// function getCookie(name) {
+//   const cDecoded = decodeURIComponent(document.cookie);
+//   const cArray = cDecoded.split(";");
+//   console.log(cArray);
+//   let result = null;
 
-  cArray.forEach((element) => {
-    if (element.indexOf(name) == 0) {
-      result = element.substring(name.length + 1);
-    }
-  });
-  return result;
-}
+//   cArray.forEach((element) => {
+//     if (element.indexOf(name) == 0) {
+//       result = element.substring(name.length + 1);
+//     }
+//   });
+//   return result;
+// }
 
+// interractive stopwatch
+const timeDisplay = document.querySelector("#timeDisplay");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
 
-
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalid;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
