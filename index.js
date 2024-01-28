2204e0abb0e087573272693ade26372116b3e5c6
Blessing Awak <melissasignature1@gmail.com>
2024-01-28 19:07:23 +0100

    commit

--- index.js
+++ index.js
@@ -1460,7 +1460,7 @@ let startTime = 0;
 let elapsedTime = 0;
 let currentTime = 0;
 let paused = true;
-let intervalid;
+let intervalId;
 let hrs = 0;
 let mins = 0;
 let secs = 0;
@@ -1469,7 +1469,22 @@ startBtn.addEventListener("click", () => {
   if (paused) {
     paused = false;
     startTime = Date.now() - elapsedTime;
-    intervalid = setInterval(updateTime, 1000);
+    intervalId = setInterval(updateTime, 1000);
   }
 });
-pauseBtn.addEventListener("click", () => {})
+pauseBtn.addEventListener("click", () => {});
+resetBtn.addEventListener("click", () => {});
+
+function updateTime() {
+  elapsedTime = Date.now() - startTime;
+
+  secs = Math.floor((elapsedTime / 1000) % 60);
+  mins = Math.floor(((elapsedTime / 1000) * 60) % 60);
+  hrs = Math.floor(((elapsedTime / 1000) * 60 * 60) % 60);
+
+  timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+  function pad(unit) {
+    return ("0" + unit).length > 2 ? unit : "o" + unit;
+  }
+}
