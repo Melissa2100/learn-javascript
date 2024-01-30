df6d335589c46f146708ee108b36f7380c52adc9
Blessing Awak <melissasignature1@gmail.com>
2024-01-30 18:01:02 +0100

    commit

--- index.js
+++ index.js
@@ -1511,3 +1511,33 @@
 // }
 
 // Game - Rock, Paper, Scissors
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach((button) =>
+  button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+  })
+);
+
+function computerTurn() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+
+  switch (random) {
+    case 1:
+      computer = "ROCK";
+      break;
+    case 2:
+      computer = "PAPER";
+      break;
+    case 3:
+      computer = "SCISSORS";
+      break;
+  }
+}
